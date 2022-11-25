7b8d63b82c1c3fd16d74efacbf3a775564c2754a
Karim El Kholy <karim.elkholy@studio.unibo.it>
2022-11-25 18:02:23 +0100

    Esercizio 1 completato

--- 51-set/src/main/java/it/unibo/collections/sets/UseSet.java
+++ 51-set/src/main/java/it/unibo/collections/sets/UseSet.java
@@ -1,6 +1,11 @@
 package it.unibo.collections.sets;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+import javax.lang.model.element.Element;
 
 /**
  * Example class using {@link java.util.Set}.
@@ -24,22 +29,42 @@ public final class UseSet {
          *
          * 1) Builds a TreeSet containing Strings
          */
+        final Collection<String> treeSet = new TreeSet<>();
         /*
          * 2) Populates such Collection with all the Strings representing numbers ranging from "1" to
          * "20" (both included)
          */
+        final int MAX_POPULATION = 20;
+        for (int i = 1; i <= MAX_POPULATION; i++ ) {
+            treeSet.add(Integer.toString(i));
+        }
         /*
          * 3) Prints its content
          */
+        System.out.println(treeSet);
         /*
          * 4) Removes all those strings whose represented number is divisible by three.
          * Note: the method removeIf(Predicate) is not allowed.
          */
+
+        Iterator<String> sI = treeSet.iterator();
+        while (sI.hasNext()) {
+            if (Integer.valueOf(sI.next()) % 3 == 0)
+            sI.remove();
+        }
         /*
          * 5) Prints the content of the Set using a for-each construct
          */
+        for (var element : treeSet) {
+            System.out.println(element);
+        }
         /*
          * 6) Verifies whether all the numbers left in the set are even
          */
+        for (var element : treeSet) {
+            if (Integer.valueOf(element) % 2 == 0){
+                System.out.println("["+element+"]"+"Even!");
+            }
+        }
     }
 }
